352cf929ef09c55f109ba10a4b1526dd815a9a07
Amit Saggar <amit.saggar@move.com>
2018-11-17 13:15:25 -0800

    paint fence

--- house-paint-dp2.js
+++ house-paint-dp2.js
@@ -22,5 +22,5 @@ var minCost = function(costs) {
       costs[i][1] += Math.min(costs[i-1][0], costs[i-1][2]);
       costs[i][2] += Math.min(costs[i-1][0], costs[i-1][1]);
   }
-  return Math.min( Math.min(costs[costs.length-1][0],costs[costs.length-1][2]), costs[costs.length-1][0] );
+  return Math.min( Math.min(costs[costs.length-1][0],costs[costs.length-1][1]), costs[costs.length-1][2] );
 };
