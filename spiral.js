839e489ffca51407f55b83967726509203e128c2
Amit Saggar <amit.saggar@move.com>
2019-08-21 14:57:05 -0700

    Fix all issues in spiral matrix

--- spiral.js
+++ spiral.js
@@ -1,3 +1,60 @@
+/**
+ * Problem we were working on -- 
+ * Issue in the code we were working on - we only had 9 chances , 9 loops in case of (3*3). 
+ * first one the (r,c) is becoming (0,0). Hence its not going further and outer loop also exists and it never went inside. 
+ * I am still solving the problem, and will very soon resolve it.
+*/ 
+var fillMatrix = function(n) {
+
+  if(n == 0) {
+      return [];
+  }
+
+  let matrix = new Array(n).fill(0);
+
+  for(let i=0; i<n; i++) {
+      matrix[i] = new Array(n).fill(0);
+  }
+
+
+  let dirs = [ [0,1], [1,0], [0,-1], [-1,0] ];
+  let length = matrix.length * matrix[0].length;
+  let r = 0;
+  let c = 0;
+  let d = 0;
+  let num = 1;
+
+  while(num <= length) {
+      for(let i = 0; i < n ; i++, num++) {
+          // console.log(r+','+c + ', n:'+n +', d:'+d);
+          matrix[r][c] = num;
+          r += dirs[d][0];
+          c += dirs[d][1];
+      }
+      // console.log('end of loop');
+      r -= dirs[d][0];
+      c -= dirs[d][1];
+      d = (d + 1) % 4;
+      if(d % 2 != 0) {
+          n = n-1;
+      }
+      r += dirs[d][0];
+      c += dirs[d][1];
+
+  }
+
+  return matrix;
+};
+
+fillMatrix(2).forEach((ele) => console.log(ele));
+fillMatrix(3).forEach((ele) => console.log(ele));
+fillMatrix(4).forEach((ele) => console.log(ele));
+
+/**
+ * 
+ * New solution which is much simpler. 
+ * I wish i had tried it first time.
+ */
 var fillMatrixSimplified = function(n) {
   if(n == 0) {
     return [];
@@ -36,44 +93,6 @@ var fillMatrixSimplified = function(n) {
   return matrix;
 };
 
-fillMatrixSimplified(2).forEach((ele) => console.log(ele));
-fillMatrixSimplified(3).forEach((ele) => console.log(ele));
-fillMatrixSimplified(4).forEach((ele) => console.log(ele));
-
-var fillMatrix = function(n) {
-  
-  if(n == 0) {
-    return [];
-  }
-  
-  let matrix = new Array(n).fill(0);
-  
-  for(let i=0; i<n; i++) {
-    matrix[i] = new Array(n).fill(0);
-  }
-  
-  let dirs = [ [0,1], [1,0], [0,-1], [-1,0] ];
-  let length = matrix.length * matrix[0].length;
-  let r = 0;
-  let c = 0;
-  let d = 0
-  let num = 1;
-  
-  while(num <= length) {
-    for(let i = 0; i < n-1; i++, num++) {
-      matrix[r][c] = num;
-      r += dirs[d][0];
-      c += dirs[d][1];
-    }
-    // if(matrix[r][c] != 0) {
-    //   n = n-1;
-    //   continue;
-    // }
-    d = (d + 1) % 4
-
-  }
-  
-  return matrix;
-};
-
-fillMatrix(2).forEach((ele) => console.log(ele));
+// fillMatrixSimplified(2).forEach((ele) => console.log(ele));
+// fillMatrixSimplified(3).forEach((ele) => console.log(ele));
+// fillMatrixSimplified(4).forEach((ele) => console.log(ele));
